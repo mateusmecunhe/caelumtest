6bcf481a4d70c23ebd554125159cd5d696174c29
mateusmecunhe <mateusmecunhe@gmail.com>
2018-11-27 21:37:40 -0200

    test

---
+++ js/opcoesdapagina/removeCartaoV2.js
@@ -0,0 +1,14 @@
+;(function(){
+const btnRemove = document.querySelectorAll(".opcoesDoCartao-remove") /* querySelectorAll retorna um array */
+
+btn.forEach(removeCard)
+function removeCard(){
+    btnRemove.addEventListener("click", function(
+        const cartao = btn.parentNode.parentNode
+        cartao.classList.add("cartao--some")
+        cartao.addEventListener("transitionend", function(
+            cartao.remove()
+        ))
+    ) )
+})
+})()
